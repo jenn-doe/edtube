febd1196df996e3c62b350b7770300c962d71950
Ben <benblinn@hotmail.com>
2018-11-11 14:27:55 -0800

    first SQL queries

--- src/routes/example.js
+++ src/routes/example.js
@@ -30,15 +30,52 @@ router.get('/', (req, res, next) => {
 
 function insertPostalCode(postalcode, city, province) {
   var insertPostalCode = `INSERT INTO PostalCode VALUES
-    ('${postalcode}', '${city}', '${province}');`
+    ('${postalcode}', '${city}', '${province}');`;
   return db.any(insertPostalCode);
 }
 
+function insertNewUser(uName, bio, name, email, address, postalCode) {
+    var insertNewUser = `INSERT INTO TubeUser VALUES
+    ('${uName}', '${bio}', '${name}', '${email}', '${address}', '${postalCode}');`;
+    return db.any(insertNewUser);
+}
+
+function updateBiography(uName, bio) {
+    var updateBiography = `UPDATE TubeUser SET biography = '${bio}' WHERE uName = '${uName}';`;
+    return db.any(updateBiography);
+}
+
+function deleteUserVideos(uName) {
+    var deleteUserVideos = `DELETE FROM Video_PostedAt_Contains WHERE uName = '${uName}';`;
+    return db.any(deleteUserVideos);
+}
+
 router.post('/', (req, res, next) => {
-  console.log('POST example');
-  insertPostalCode(req.body.postalcode, req.body.city, req.body.province)
-    .then(getDataAndRender(res))
-    .catch((err) => console.log(err));
+  console.log(req.body);
+  let html_keys = Object.keys(req.body);
+  let operation = html_keys[html_keys.length - 1];
+  console.log(operation);
+
+  switch (operation) {
+      case "insertPCode" : insertPostalCode(req.body.postalcode, req.body.city, req.body.province)
+          .then(getDataAndRender(res))
+          .catch((err) => console.log(err));
+      break;
+      case "insertUser" : insertNewUser(req.body.uName, req.body.bio, req.body.name, req.body.email,
+          req.body.address, req.body.postalCode)
+          .then(getDataAndRender(res))
+          .catch((err) => console.log(err));
+      break;
+      case "updateBiography" : updateBiography(req.body.uName, req.body.bio)
+          .then(getDataAndRender(res))
+          .catch((err) => console.log(err));
+      break;
+      case "deleteUserAllVideos" : deleteUserVideos(req.body.uName)
+          .then(getDataAndRender(res))
+          .catch((err) => console.log(err));
+      break;
+  }
+
 });
 
 module.exports = router;
