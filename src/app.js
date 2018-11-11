a968cd5d5b308edd8154e70eb0b65156ddd9329d
Jennifer MacFarlane <jennmacfarlane@Jennifers-MBP.hitronhub.home>
2018-11-10 17:46:57 -0800

    set up front end with sass and a nav bar

--- src/app.js
+++ src/app.js
@@ -1,24 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const index = require('./routes/index');
-const example = require('./routes/example');
-const exphbs  = require('express-handlebars');
-const createData = require('./createData');
+const express = require("express");
+const bodyParser = require("body-parser");
+const index = require("./routes/index");
+const example = require("./routes/example");
+const exphbs = require("express-handlebars");
+const sassMiddleware = require("node-sass-middleware");
+const createData = require("./createData");
+const path = require("path");
 
 const app = express();
 const port = 8000;
 
-app.engine('hbs', exphbs());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hbs');
+app.use(
+  sassMiddleware({
+    src: __dirname,
+    dest: __dirname,
+    debug: true,
+    outputStyle: "compressed",
+    force: true
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("hbs", exphbs());
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', index);
-app.use('/example', example);
+app.use("/", index);
+app.use("/example", example);
 
 createData();
 
 app.listen(port);
-console.log('Server listening on port ' + port);
-
+console.log("Server listening on port " + port);
