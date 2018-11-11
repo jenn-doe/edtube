26e6495006fcd39e6ed941e862e4df5a6b5f922a
Jennifer MacFarlane <jennmacfarlane@dhcp-128-189-200-66.ubcsecure.wireless.ubc.ca>
2018-11-11 14:25:07 -0800

    added UI for user, playlist, trending routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 let router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET index');
-  res.redirect('/example'); 
+router.get("/", (req, res, next) => {
+  console.log("GET index");
+  res.redirect("/user");
 });
 
 module.exports = router;
