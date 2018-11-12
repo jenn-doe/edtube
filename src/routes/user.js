db7a6270757f42a121361f6ae03dd457a66715bb
Ben <benblinn@hotmail.com>
2018-11-11 18:05:19 -0800

    currently broke (delete video)

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,18 +20,41 @@ function updateBiography(uName, bio) {
 }
 
 function deleteUserVideos(uName) {
-    var deleteUserVideos = `DELETE FROM Video_PostedAt_Contains WHERE uName = '${uName}';`;
+    var deleteUserVideos = `DELETE FROM Video_PostedAt_Contains 
+    WHERE cName 
+    IN (SELECT cName 
+        FROM Channel_Owns_BelongsTo
+        WHERE uName= '${uName}');`;
+
     return db.any(deleteUserVideos);
 }
+
+// function viewUserVideos(uName) {
+//     var viewUserVideos = `CREATE OR REPLACE VIEW videos AS
+//     SELECT * FROM Video_PostedAt_Contains
+//     WHERE cName IN (SELECT cName FROM Channel_Owns_BelongsTo
+//                     WHERE uName = '${uName}');`;
+//     return db.any(viewUserVideos);
+// }
 
 function getDataAndRender(res) {
   Promise.all([getUsers()])
     .then(([users]) => {
       res.render('user', {
-        users: users
+        users: users,
+          videos: null
       });
   });
 }
+
+// function getDataAndRenderVideos(res) {
+//     Promise.all([getVidoes()])
+//         .then(([videos]) => {
+//             res.render('videos', {
+//                 users: videos
+//             });
+//         });
+// }
 
 router.get('/', (req, res, next) => {
   console.log('GET user');
@@ -47,17 +70,28 @@ router.post('/', (req, res, next) => {
   switch (operation) {
     case "insert-user" : insertNewUser(req.body["input-username"], req.body["input-biography"], req.body["input-name"],
             req.body["input-email"], req.body["input-address"], req.body["input-postalcode"])
-            .then(getDataAndRender(res))
-            .catch((err) => console.log(err));
+            .then(getUsers)
+            .then(users => {
+                res.render('user', {
+                    users: users,
+                    videos: null
+                })});
     break;
     case "update-bio" : updateBiography(req.body["input-username"], req.body["input-bio"])
-            .then(getDataAndRender(res))
-            .catch((err) => console.log(err));
+            .then(getUsers)
+            .then(users => {
+                res.render('user', {
+                    users: users,
+                    videos: null
+                })});
     break;
     case "delete-user-vids" : deleteUserVideos(req.body["input-username"])
-            .then(getDataAndRender(res))
-            .catch((err) => console.log(err));
+        .then();
     break;
+      // case "get-videos-user" : viewUserVideos(req.body["input-username"])
+      //     .then(getDataAndRender(res))
+      //     .catch((err) => console.log(err));
+      // break;
   }
 });
 
