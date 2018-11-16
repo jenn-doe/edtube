85efa2b7e2ad97d958b266074926cfe9fdef10ba
Jennifer MacFarlane <jenn-doe@users.noreply.github.com>
2018-11-16 15:54:51 -0800

    finished playlist error handling, added UI for error handling in playlists, users, trending

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const db = require('../db');
+const express = require("express");
+const db = require("../db");
 
 let router = express.Router();
 
@@ -9,15 +9,17 @@ function getPlaylist() {
 }
 
 function deletePlaylist(pName, uName) {
-    var playlistToDelete = `DELETE FROM Playlist_Creates WHERE pName = '${pName}' AND uName = '${uName}' returning uName;`;
-    return db.oneOrNone(playlistToDelete);
+  var playlistToDelete = `DELETE FROM Playlist_Creates 
+    WHERE pName = '${pName}' AND uName = '${uName}' 
+    RETURNING uName;`;
+  return db.oneOrNone(playlistToDelete);
 }
 
 function getVideosInPlaylist(pName, uName) {
   var playlistToGet = `SELECT Video_PostedAt_Contains.vID AS vID, Video_PostedAt_Contains.description, Video_PostedAt_Contains.playtime
     FROM Video_PostedAt_Contains
     INNER JOIN PartOf ON PartOf.vID=Video_PostedAt_Contains.vID
-    WHERE PartOf.uName = '${uName}' AND PartOf.pName = '${pName}'`;
+    WHERE PartOf.uName = '${uName}' AND PartOf.pName = '${pName}';`;
   return db.any(playlistToGet);
 }
 
@@ -30,71 +32,97 @@ function getNumVideosPerChannel(pName, uName) {
   return db.any(playlistToGetChannelsFor);
 }
 
-router.get('/', (req, res, next) => {
-  console.log('GET playlist');
-  getPlaylist()
-    .then(allPlaylists => {
-      res.render('playlist', {
-        numPerChan: null,
-        vidsInPlaylist: null,
-        allPlaylists: allPlaylists
-      })})
+router.get("/", (req, res, next) => {
+  getPlaylist().then(allPlaylists => {
+    res.render("playlist", {
+      numPerChan: null,
+      vidsInPlaylist: null,
+      allPlaylists: allPlaylists,
+      error: null
+    });
+  });
 });
 
-router.post('/', (req, res, next) => {
-  console.log('POST example');
-
+router.post("/", (req, res, next) => {
   let html_keys = Object.keys(req.body);
-  console.log(html_keys);
   let operation = html_keys[html_keys.length - 1];
 
   switch (operation) {
-    case "get-num-vids-per-channel-in-playlist" :
+    case "get-num-vids-per-channel-in-playlist":
       getNumVideosPerChannel(req.body["input-pname"], req.body["input-uname"])
-            .then(numPerChan => {
-              res.render('playlist', {
-                numPerChan: numPerChan,
-                vidsInPlaylist: null,
-                allPlaylists: null
-              })})
-            .catch((err) => console.log(err));
-    break;
-    case "get-vids-from-playlist" :
+        .then(numperChan => {
+          if (numperChan.length < 1) {
+            throw new Error();
+          } else {
+            res.render("playlist", {
+              numPerChan: numperChan,
+              vidsInPlaylist: null,
+              allPlaylists: null,
+              error: null
+            });
+          }
+        })
+        .catch(err => {
+          res.render("playlist", {
+            users: null,
+            videos: null,
+            followers: null,
+            error:
+              "There are no videos in this playlist. If you think this is an error, please ensure you have the correct username and playlist."
+          });
+        });
+      break;
+    case "get-vids-from-playlist":
       getVideosInPlaylist(req.body["input-pname"], req.body["input-uname"])
-            .then(vidsInPlaylist => {
-              res.render('playlist', {
-                numPerChan: null,
-                vidsInPlaylist: vidsInPlaylist,
-                allPlaylists: null
-              })})
-            .catch((err) => console.log(err));
-    break;
-    case "delete-playlist" :
+        .then(vidsInPlaylist => {
+          if (vidsInPlaylist.length < 1) {
+            throw new Error();
+          } else {
+            res.render("playlist", {
+              numPerChan: null,
+              vidsInPlaylist: vidsInPlaylist,
+              allPlaylists: null,
+              error: null
+            });
+          }
+        })
+        .catch(err => {
+          res.render("playlist", {
+            users: null,
+            videos: null,
+            followers: null,
+            error:
+              "There was an error getting the videos. Please ensure you have the correct username and playlist."
+          });
+        });
+      break;
+    case "delete-playlist":
       deletePlaylist(req.body["input-pname"], req.body["input-uname"])
-        .then((res) => {
-          console.log(res)
+        .then(res => {
           if (res == null) {
-            throw new Error
+            throw new Error();
           }
         })
         .then(getPlaylist)
-          .then(allPlaylists => {
-            res.render('playlist', {
-              numPerChan: null,
-              vidsInPlaylist: null,
-              allPlaylists: allPlaylists
-            })})
-          .catch((err) => {
-              console.log("there was an error", err)
-              res.render('playlist', {
-                  users: null,
-                  videos: null,
-                  followers: null,
-                  error: "There was an error deleting the playlist. Please ensure you have the correct username and playlist."
-              })});
-    break;
+        .then(allPlaylists => {
+          res.render("playlist", {
+            numPerChan: null,
+            vidsInPlaylist: null,
+            allPlaylists: allPlaylists,
+            error: null
+          });
+        })
+        .catch(err => {
+          res.render("playlist", {
+            users: null,
+            videos: null,
+            followers: null,
+            error:
+              "There was an error deleting the playlist. Please ensure you have the correct username and playlist."
+          });
+        });
+      break;
   }
-
 });
 
 module.exports = router;
